1d193cca9f05cc9f685083fc5199509e04b78c2f
Hei34 <76108392+Hei34@users.noreply.github.com>
2023-05-17 15:18:49 +0200

    Add files via upload

--- perfumy.pl
+++ perfumy.pl
@@ -9,6 +9,10 @@ perfumy(wisnia, kwiatowe, swieze, damskie).
 perfumy(sandaly, drzewne, ekscentryczne, meskie).
 perfumy(olej_rzep, kwiatowe, lagodne, damskie).
 perfumy(owocelesne, owocowe, intensywne, unisex).
+perfumy(black_orchid, kwiatowe, intensywne, damskie).
+perfumy(velvet_rose, kwiatowe, lagodne, damskie).
+perfumy(citrus_splash, owocowe, swieze, unisex).
+
 
 %regu³y do wyszukiwañ
 znajdz_dla_plec(Perfumy, Plec):-
@@ -18,6 +22,7 @@ znajdz_dla_zapach(Perfumy, Zapach):-
 wyswietl_wszystkie(Perfum):-
     perfumy(Perfum,_,_,_).
 %regu³y do wyœwietlania z menu
+%
 wyswietl_wedlug_plec(Plec):-
     perfumy(Perfum,_,_,Plec),
     write(Perfum),
@@ -28,6 +33,7 @@ wyswietl_wedlug_intensywnosc(Intensywnosc,Plec):-
     write(Perfum),
     nl,
     fail.
+
 wyswietl_wedlug_pochodzenia(Pochodzenie,Plec):-
     perfumy(Perfum,Pochodzenie,_,Plec),
     write(Perfum),
@@ -45,15 +51,15 @@ menu :-
     wykonaj(Opcja).
 
 wykonaj(1) :-
-    write('Wybrano opcjê pierwsza.'), nl,
+    write('ZENSKIE.'), nl,
     menu_druga_opcja(damskie).
 
 wykonaj(2) :-
-    write('Wybrano opcjê druga.'), nl,
+    write('MESKIE.'), nl,
     menu_druga_opcja(meskie).
 
 wykonaj(3) :-
-    write('Wybrano opcjê trzeciš.'), nl,
+    write('UNISEX'), nl,
     menu_druga_opcja(unisex).
 
 menu_druga_opcja(Plec) :-
@@ -69,14 +75,15 @@ wykonaj_druga_opcja(1 ,Plec) :-
     wyswietl_wedlug_plec(Plec).
 
 wykonaj_druga_opcja(2, Plec) :-
-    write('Wybrano opcjê B.'), nl,
-    write(Plec).
+    pomaluj("WEDLUG POCHODZENIA:"),
+    menu_pochodzenie(Plec).
 
 wykonaj_druga_opcja(3, Plec) :-
-    write('Wybrano opcjê C.'), nl,
+    pomaluj("WEDLUG INTENSYWNOSCI:"),
     menu_zapach(Plec).
 
-%todo cofanie w menu,menu pochodzenia, dokonczyc menu zapachu
+% todo cofanie w menu,
+% komentarze do funkcji i menu
 menu_zapach(Plec) :-
     write('1. Pokaz wszystkie'), nl,
     write('2. Lagodne'), nl,
@@ -88,15 +95,62 @@ menu_zapach(Plec) :-
     wykonaj_trzecia_opcja(Opcja, Plec).
 
 wykonaj_trzecia_opcja(1 ,Plec) :-
-    write('Wszystkie.'), nl,
+    pomaluj("==WSZYSTKIE=="),
     wyswietl_wedlug_plec(Plec).
 
 wykonaj_trzecia_opcja(2, Plec) :-
-    write('Wybrano opcjê B.'), nl,
+    pomaluj('==LAGODNE=='),
     wyswietl_wedlug_intensywnosc(lagodne,Plec).
 
 wykonaj_trzecia_opcja(3, Plec) :-
-    write('Wybrano opcjê C.'), nl,
+    pomaluj('==INTENSYWNE=='),
     wyswietl_wedlug_intensywnosc(intensywne,Plec).
+
+wykonaj_trzecia_opcja(4, Plec) :-
+    pomaluj('==EKSCENTRYCZNE=='),
+    wyswietl_wedlug_intensywnosc(ekscentryczne,Plec).
+
+wykonaj_trzecia_opcja(5, Plec) :-
+    pomaluj("==SWIERZE=="),
+    wyswietl_wedlug_intensywnosc(swierze,Plec).
+
+%funkcja do kolorowania i wyœwietlania tekstu
+pomaluj(String):-
+    nl,
+    format('\e[35m'),
+    write(String),
+    format('\e[0m'),
+    nl.
+
+menu_pochodzenie(Plec):-
+    write('1. Pokaz wszystkie'), nl,
+    write('2. Owocowe'), nl,
+    write('3. Drzewne'), nl,
+    write('4. Kwiatowe'), nl,
+    write('5. Korzenne'), nl,
+    write('Wybierz opcjê (wpisz numer): '),
+    read(Opcja),
+    wykonaj_czwarta_opcja(Opcja, Plec).
+
+wykonaj_czwarta_opcja(1, Plec):-
+    pomaluj("==WSZYSTKIE=="),
+    wyswietl_wedlug_plec(Plec).
+
+wykonaj_czwarta_opcja(2, Plec):-
+    pomaluj("==OWOCOWE=="),
+    wyswietl_wedlug_pochodzenia(owocowe,Plec).
+
+wykonaj_czwarta_opcja(3, Plec):-
+    pomaluj("==DRZEWNE=="),
+    wyswietl_wedlug_pochodzenia(drzewne,Plec).
+
+wykonaj_czwarta_opcja(4, Plec):-
+    pomaluj("==KWIATOWE=="),
+    wyswietl_wedlug_pochodzenia(kwiatowe,Plec).
+
+wykonaj_czwarta_opcja(5, Plec):-
+    pomaluj("==KORZENNE=="),
+    wyswietl_wedlug_pochodzenia(korzenne,Plec).
+
 
 
